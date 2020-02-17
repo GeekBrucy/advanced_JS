4dda7a678023abed88d4c46d82442e92bef0d860
GeekBrucy <peichunzhou1006@gmail.com>
2020-02-17 22:49:55 +1100

    complete set

--- data_structure/05collection.js
+++ data_structure/05collection.js
@@ -27,6 +27,52 @@ var MySet = function(){
   this.size = function(){
     return Object.keys(items).length;
   }
+  this.value = function(){
+    var values = [];
+    for(var i in items){
+      if(items.hasOwnProperty(i)){
+        values.push(i)
+      }
+    }
+    return values;
+  }
+  this.union = function(otherSet){
+    var resultSet = new MySet();
+
+    var arr = this.value();
+    for(var i = 0; i < arr.length; i++){
+      resultSet.add(arr[i]);
+    }
+
+    arr = otherSet.value();
+    for(var i = 0; i < arr.length; i++){
+      resultSet.add(arr[i]);
+    }
+    return resultSet;
+  }
+  this.intersection = function(otherSet){
+    var resultSet = new MySet();
+
+    var arr = this.value();
+    for(var i = 0; i < arr.length; i++){
+      if(otherSet.has(arr[i])){
+        resultSet.add(arr[i]);
+      }
+    }
+    return resultSet;
+  }
+  this.diff = function(otherSet){
+    var resultSet = new MySet();
+
+    var arr = this.value();
+
+    for(var i = 0; i < arr.length; i++){
+      if(!otherSet.has(arr[i])){
+        resultSet.add(arr[i]);
+      }
+    }
+    return resultSet;
+  }
   this.getItems = function(){
     return items;
   }
